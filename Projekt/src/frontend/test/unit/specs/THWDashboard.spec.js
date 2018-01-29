6295666fc02ff61e14a9b9427acee9c151aba97c
Anja Sieke <as75ropo@studserv.uni-leipzig.de>
2018-01-29 15:11:36 +0100

    Addes tests

---
+++ Projekt/src/frontend/test/unit/specs/THWDashboard.spec.js
@@ -0,0 +1,34 @@
+import { shallow, createLocalVue } from 'vue-test-utils'
+import THWDashboard from '@/components/THWDashboard'
+import expect from 'expect'
+import Vuex from 'vuex'
+import store from '@/store'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('THWDashboard', () => {
+
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(THWDashboard, { store } )
+  })
+
+  it('displays the author', () => {
+    expect(wrapper.html()).toContain('prop="hdzIncomingA"')
+  })
+
+  it('displays the creation date', () => {
+    expect(wrapper.html()).toContain('prop="dateIncomingA"')
+  })
+
+  it('displays the creation time', () => {
+    expect(wrapper.html()).toContain('prop="timeIncomingA"')
+  })
+
+  it('displays the content of a ticket', () => {
+    expect(wrapper.html()).toContain('prop="message"')
+  })
+
+})
---
+++ Webseiten/_posts/2018-01-29-entwurf-online.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title:  "Entwurfsbeschreibung und Demo jetzt online"
+date:   2018-01-29 16:22:34 +0100
+categories: jekyll update
+---
+
+[Hier](http://pcai042.informatik.uni-leipzig.de/~na17b/entwurf.pdf) findet ihr unsere Entwurfsbeschreibung für das Vorprojekt und hier ein [Link](http://pcai042.informatik.uni-leipzig.de/~na17b/thw) zur Demo.
