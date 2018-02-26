c3b876994138152411f7ec7f38a87160bff73eee
Marc Wahsner <mw83waji@studserv.uni-leipzig.de>
2018-02-26 11:32:16 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,2 @@
-var a,b;                  // explizite Deklaration einer globalen Variable
+var a;                  // explizite Deklaration einer globalen Variable
 a = 1;                    // Definition einer Variable
-document.write(a);        // ergibt 1
