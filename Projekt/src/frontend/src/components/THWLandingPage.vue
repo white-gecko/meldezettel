45480a0a68b73937588a8d999b7c4b233002ffed
Lukas Gehrke <lg58weky@studserv.uni-leipzig.de>
2018-03-10 16:18:06 +0100

    Edits order in THWLandingPage, adds spacing. Edits function setUserData in mutations.js. Changes userData to empty object in state.js

--- Projekt/src/frontend/src/components/THWLandingPage.vue
+++ Projekt/src/frontend/src/components/THWLandingPage.vue
@@ -1,44 +1,47 @@
 <!-- this component will be our landing page -->
+import { mapMutations } from 'vuex';
 <!-- landing page will demand Rolle, Name, Funktion, Hdz -->
 <template>
   <div>
+    <el-dropdown @command="this.setRole" style="margin-bottom: 20px">
+      <span class="el-dropdown-link">
+        Rolle auswählen<i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item command="Sichter">Sichter</el-dropdown-item>
+        <el-dropdown-item command="Funker">Funker</el-dropdown-item>
+        <el-dropdown-item command="LdF">LdF</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
     <!-- use element-UI elements to design the component -->
     <el-input
       placeholder="Name"
-      v-model="input1"
+      v-model="formdata.name"
+      style="margin-bottom: 20px"
       :disabled="false">
     </el-input>
 
-    <landingPage-item>
     <el-input
       placeholder="Handzeichen"
-      v-model="input1"
+      v-model="formdata.signature"
+      style="margin-bottom: 20px"
       :disabled="false">
     </el-input>
-    </landingPage-item>
 
     <el-input
       placeholder="Funktion"
-      v-model="input1"
+      v-model="formdata.position"
+      style="margin-bottom: 20px"
       :disabled="false">
     </el-input>
 
-    <el-dropdown @command="this.setRole">
-    <span class="el-dropdown-link">
-      Rolle auswählen<i class="el-icon-arrow-down el-icon--right"></i>
-    </span>
-    <el-dropdown-menu slot="dropdown">
-      <el-dropdown-item command="Sichter">Sichter</el-dropdown-item>
-      <el-dropdown-item command="Funker">Funker</el-dropdown-item>
-      <el-dropdown-item command="LdF">LdF</el-dropdown-item>
-    </el-dropdown-menu>
-  </el-dropdown>
-
-  <el-button>Bestätigen</el-button>
+  <el-button @click="this.setUserData">Bestätigen</el-button>
   </div>
 </template>
 
 <script>
+
+import { mapMutations } from 'vuex'
 // define data function to return stored data
 // define method to store data inside store/state.js
 export default {
@@ -47,16 +50,24 @@ export default {
 
   data: () => {
     return {
-      userData: {
+      formdata: {
         role: '',
         name: '',
         position: '',
         signature: ''
       }
     }
-  }
+  },
 
+  methods: {
+
+    ...mapMutations(['setUserData']),
+
+    ...mapMutations(['setRole'])
+
+  }
 }
+
 </script>
 
 <style>
--- Projekt/src/frontend/src/store/state.js
+++ Projekt/src/frontend/src/store/state.js
@@ -1,4 +1,5 @@
 export default {
   role: 'Sichter',
-  ticketlist: []
+  ticketlist: [],
+  userdata: {}
 }
