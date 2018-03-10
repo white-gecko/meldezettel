acd70df9b1fded473f21b6e7d568dd4304bfbf33
Lukas Gehrke <lg58weky@studserv.uni-leipzig.de>
2018-03-10 13:11:04 +0100

    Adds THWLandingPage.vue to src/components.

---
+++ Projekt/src/frontend/src/components/THWLandingPage.vue
@@ -0,0 +1,14 @@
+<!-- this component will be our landing page -->
+<!-- landing page will demand Rolle, Name, Funktion, Hdz -->
+<template>
+  <div>
+    <!-- use element-UI elements to design the component -->
+
+  </div>
+</template>
+
+<script>
+  // define data function to return stored data
+  // define method to store data inside store/state.js
+
+</script>
