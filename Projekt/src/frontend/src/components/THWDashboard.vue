e95bdc4d8bdafced1a7ae8e435e31f012dff1bb5
Max Schrodt <ms18qame@studserv.uni-leipzig.de>
2018-03-10 11:17:56 +0100

    refactored THWDashboard.vue

--- Projekt/src/frontend/src/components/THWDashboard.vue
+++ Projekt/src/frontend/src/components/THWDashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-table :data="td" style="width: 100%" max-height="500">
+  <el-table :data="tabledata" style="width: 100%" max-height="500">
 
     <el-table-column width="150">
       <template slot-scope="scope">
@@ -21,79 +21,65 @@
 
 <script>
 
-import { mapGetters, mapMutations } from 'vuex'
+import { mapGetters } from 'vuex'
 import { quitstore } from '../api/QuitStoreAdapter.js'
 import { handleResponse } from '../sparql_help/response_to_formdata.js'
 import sparql from '../sparql_help/sparql_queries.js'
 
-import store from '../store/state.js'
+// import store from '../store/state.js'
 
 export default {
 
   name: 'THWDashboard',
 
-  docs: {},
-
   beforeRouteEnter (to, from, next) {
     let query = sparql.allDocumentsQuery()
-    var callback = function (response) {
-      next(vm => vm.setDocs(response))
-    }
-    quitstore.getData(query).then(callback)
+    quitstore.getData(query)
+      .then((response) => {
+        next(vm => {
+          let data = handleResponse(response.data)
+          vm.setData(data)
+          vm.$store.dispatch('setTicketlist', data)
+        })
+      })
+      .catch((error) => {
+        alert(error)
+        next()
+      })
   },
 
   beforeRouteUpdate (to, from, next) {
+    console.log('update')
     let query = sparql.allDocumentsQuery()
-    var callback = function (response) {
-      this.setDocs(response)
-      next()
-    }
-    quitstore.getData(query).then(callback)
+    quitstore.getData(query)
+      .then((response) => {
+        let data = handleResponse(response.data)
+        this.setData(data)
+        this.$store.dispatch('setTicketlist', data)
+        next()
+      })
+      .catch((error) => {
+        alert(error)
+        next()
+      })
   },
 
   data () {
     return {
-      td: [{}]
+      tabledata: [{}]
     }
   },
 
   methods: {
-    setDocs (d) {
-      let formdatas = handleResponse(d.data)
-      this.$data.td = formdatas
-      store.ticketlist = formdatas
+    setData (data) {
+      this.$data.tabledata = data
     }
   },
 
   computed: {
     ...mapGetters({
       ticketlist: 'getAllTickets'
-    }),
-
-    ...mapMutations({
-      setTicketlist: 'setTicketlist'
-    }),
-
-    tabledata: function () {
-      let data = JSON.parse(JSON.stringify(this.ticketlist))
-
-      // The times and dates need to be filtered to be displayed correctly
-      for (let i = 0; i < data.length; i++) {
-      // Retrieve all required date strings and convert them to a shorter form (german style)
-        let tmpDate = new Date(data[i].dateIncomingA)
-        data[i].dateIncomingA = tmpDate.toLocaleDateString()
-
-        tmpDate = new Date(data[i].timeIncomingA)
-        data[i].timeIncomingA = tmpDate.toLocaleTimeString()
-
-        // If the message is longer than a threshold, shorten it and add dots to indicate
-        if (data[i].message.length > 80) {
-          data[i].message = data[i].message.slice(0, 77) + '...'
-        }
-      }
-
-      return data
-    }
+    })
   }
 }
 
