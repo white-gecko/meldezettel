f7508a62acd7d30ecac5bf44d9d0897993846ef8
Max Schrodt <ms18qame@studserv.uni-leipzig.de>
2018-03-15 15:57:53 +0100

    changed THWForm test to fit with the new formdata

--- Projekt/src/frontend/test/unit/specs/THWForm.spec.js
+++ Projekt/src/frontend/test/unit/specs/THWForm.spec.js
@@ -24,20 +24,20 @@ describe('THWForm', () => {
     wrapper = mount(THWForm, { store, localVue })
   })
 
-  it('binds to hdzIncomingA', () => {
-    expect(wrapper.vm.formdata.hdzIncomingA).toBe('')
+  it('binds to primaryHdZ', () => {
+    expect(wrapper.vm.formdata.primaryHdZ).toBe('')
   })
 
-  it('binds to creationTime', () => {
-    expect(wrapper.vm.formdata.timeIncomingA).toBe('')
+  it('binds to primaryTime', () => {
+    expect(wrapper.vm.formdata.primaryTime).toBe('')
   })
 
-  it('binds to creationDate', () => {
-    expect(wrapper.vm.formdata.dateIncomingA).toBe('')
+  it('binds to primaryDate', () => {
+    expect(wrapper.vm.formdata.primaryDate).toBe('')
   })
 
-  it('binds to message', () => {
-    expect(wrapper.vm.formdata.message).toBe('')
+  it('binds to content', () => {
+    expect(wrapper.vm.formdata.content).toBe('')
   })
 
   /*it('pushes a new ticket to the store', () => {
