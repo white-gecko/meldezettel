358b8d38a16ceb28e4dba1d2acd7005f70e8fe28
Max Schrodt <ms18qame@studserv.uni-leipzig.de>
2018-03-15 12:31:34 +0100

    modified THWDashboard.vue to work with previous changes

--- Projekt/src/frontend/src/components/THWDashboard.vue
+++ Projekt/src/frontend/src/components/THWDashboard.vue
@@ -3,18 +3,18 @@
 
     <el-table-column width="150">
       <template slot-scope="scope">
-        <router-link v-bind:to="{name:'Create', params:{id: scope.$index }}" tag="el-button">Öffnen</router-link>
+        <router-link v-bind:to="{name:'Create', params:{id: tabledata[scope.$index]['id'] }}" tag="el-button">Öffnen</router-link>
       </template>
     </el-table-column>
 
     <!-- Festlegen der zu verwendenden Werte aus dem VVD -->
-    <el-table-column prop="hdzIncomingA" label="Verfasser" width="100"></el-table-column>
+    <el-table-column prop="primaryHdZ" label="Verfasser" width="100"></el-table-column>
 
-    <el-table-column :formatter="formatDate" prop="dateIncomingA" label="Datum" width="100"></el-table-column>
+    <el-table-column :formatter="formatDate" prop="primaryDate" label="Datum" width="100"></el-table-column>
 
-    <el-table-column :formatter="formatTime" prop="timeIncomingA" label="Uhrzeit" width="100"></el-table-column>
+    <el-table-column :formatter="formatTime" prop="primaryTime" label="Uhrzeit" width="100"></el-table-column>
 
-    <el-table-column :formatter="formatContent" prop="message" label="Kurzinhalt"></el-table-column>
+    <el-table-column :formatter="formatContent" prop="content" label="Kurzinhalt"></el-table-column>
 
   </el-table>
 </template>
@@ -22,7 +22,7 @@
 <script>
 
 import { quitstore } from '../api/QuitStoreAdapter.js'
-import { handleResponse } from '../sparql_help/response_to_formdata.js'
+import { parseLiteralResponse } from '../sparql_help/sparql_response.js'
 import sparql from '../sparql_help/sparql_queries.js'
 
 export default {
@@ -30,12 +30,12 @@ export default {
   name: 'THWDashboard',
 
   beforeRouteEnter (to, from, next) {
-    let query = sparql.allDocumentsQuery()
+    let query = sparql.dashboardQuery()
 
     quitstore.getData(query)
       .then((response) => {
         next(vm => {
-          let data = handleResponse(response.data)
+          let data = parseLiteralResponse(response.data)
           vm.setData(data)
           vm.$store.dispatch('setTicketlist', data)
         })
@@ -46,6 +46,7 @@ export default {
       })
   },
 
+  /*
   beforeRouteUpdate (to, from, next) {
     let query = sparql.allDocumentsQuery()
 
@@ -61,7 +62,7 @@ export default {
         next()
       })
   },
-
+  */
   data () {
     return {
       tabledata: [{}]
