501d0c5f2fc383a7ad6a4794e9918f6ec59f53e4
Marc Wahsner <mw83waji@studserv.uni-leipzig.de>
2018-03-17 11:25:43 +0100

    fixed UnitTests for THWDashboard

--- Projekt/src/frontend/test/unit/specs/THWForm.spec.js
+++ Projekt/src/frontend/test/unit/specs/THWForm.spec.js
@@ -1,7 +1,8 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import THWForm from '@/components/THWForm'
-import Vuex from 'vuex'
 import expect from 'expect'
+import Vuex from 'vuex'
+
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
@@ -24,20 +25,20 @@ describe('THWForm', () => {
     wrapper = mount(THWForm, { store, localVue })
   })
 
-  it('binds to hdzIncomingA', () => {
-    expect(wrapper.vm.formdata.hdzIncomingA).toBe('')
+  it('binds to primaryHdZ', () => {
+    expect(wrapper.vm.formdata.primaryHdZ).toBe('')
   })
 
-  it('binds to creationTime', () => {
-    expect(wrapper.vm.formdata.timeIncomingA).toBe('')
+  it('binds to primaryTime', () => {
+    expect(wrapper.vm.formdata.primaryTime).toBe('')
   })
 
-  it('binds to creationDate', () => {
-    expect(wrapper.vm.formdata.dateIncomingA).toBe('')
+  it('binds to creationDate (primaryDate)', () => {
+    expect(wrapper.vm.formdata.primaryDate).toBe('')
   })
 
-  it('binds to message', () => {
-    expect(wrapper.vm.formdata.message).toBe('')
+  it('binds to message content', () => {
+    expect(wrapper.vm.formdata.content).toBe('')
   })
 
   /*it('pushes a new ticket to the store', () => {
