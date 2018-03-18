fd3a9e0e513abb5fb71947f63a7eaa7804bc6fb4
Max Schrodt <ms18qame@studserv.uni-leipzig.de>
2018-03-18 12:34:42 +0100

    resolved missed conflicts

--- Projekt/src/frontend/src/components/THWForm.vue
+++ Projekt/src/frontend/src/components/THWForm.vue
@@ -946,13 +946,10 @@ import { mapMutations, mapActions } from 'vuex'
 import { quitstore } from '../api/QuitStoreAdapter.js'
 import { parseResponse } from '../sparql_help/sparql_response.js'
 import sparql from '../sparql_help/sparql_queries.js'
-<<<<<<< HEAD
-=======
 import ElHeader from 'element-ui/packages/header/src/main'
 import ElRow from 'element-ui/packages/row/src/row'
 import ElContainer from 'element-ui/packages/container/src/main'
 import ElMain from 'element-ui/packages/main/src/main'
->>>>>>> develop
 
 export default {
 
@@ -981,13 +978,9 @@ export default {
         secondaryDate: '',
         secondaryTime: '',
         secondaryHdZ: '',
-<<<<<<< HEAD
-
-=======
         tertiaryDate: '',
         tertiaryTime: '',
         tertiaryHdZ: '',
->>>>>>> develop
         midRadio: true,
         midPhone: false,
         midFax: false,
@@ -1113,10 +1106,7 @@ export default {
 
     addFormData: function () {
       console.log(this.$data.formdata)
-<<<<<<< HEAD
-=======
       console.log(JSON.stringify(this.$data.formdata))
->>>>>>> develop
       this.$store
         .dispatch('addFormData', this.formdata)
         .then(() => this.$router.push('home'))
