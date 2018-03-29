ff83681610778a8fe6a9441fc5f8b52f4d262a3e
Marc Wahsner <mw83waji@studserv.uni-leipzig.de>
2018-03-29 17:58:43 +0200

    adds tests and doc for printservice

--- Projekt/src/print_service/printService.py
+++ Projekt/src/print_service/printService.py
@@ -1,54 +1,65 @@
+# -*- coding: iso-8859-1 -*-
 import os, io, json, re, sys
 
+#Inserts char into string at given position
 def insertChar(string, position, char):
   return string[:position] + char + string[position:]
 
+#Trims string to given length
 def trimString(string, lineLength):
   if len(string)>lineLength:
     return string[:lineLength];
   else:
     return string;
 
+#Calculates maximum length for multiLineStrings and trims String
 def adjustString(string, maxLines, lineLength):
   length = len(string)
   
+  #Calculates where Latex will linebreak
   firstPos = 0
   pos = lineLength
   usedLines = 1
   
+  #By iterating through each line, jumping to end of current line and moving backwards until spacing character is found (whitespace or hyphen)
+  #Maximum length is calculated by adding up positions of last spacing chars of each line
   while (pos < length and usedLines < maxLines):
-    print('pos ', pos,'  length ',length,'  usedLines ',usedLines,'  maxLines ',maxLines)
     while (string[pos]!=' ' and string[pos]!='-' and pos!=firstPos):
-      #print('string[pos]  ',pos, string[pos])
       pos -=1
-      
+    
+    #Catches corner case where word is longer than linelength
+    #By inserting hyphen at last position of line and therefore breaking word without attention to hyphenation 
     if (pos == firstPos):
       pos+=lineLength
       string = insertChar(string,pos-1,'-')
       length += 1
     
-    print('string ',pos, string)
     usedLines += 1
     firstPos = pos
     pos += lineLength
   
   maxLength = pos
   
+  #Trims String
+  #Adding of double backslash necessary, because multiLineStrings are single Tabularline in Latex
   if length>maxLength:
     return string[:maxLength]+"\\\\";
-  #elif (length=maxLength) and string
   else:
+    #Additional backslashes are necessary to fill Latex Tabular to predefined height
     return string + "\\\\" * (maxLines - usedLines + 1);
 
+#Makes use of regex.substitution to remove all characters that are not specified in regex below
 def removeUnwantedCharacters(s):
   #str = re.sub('[//]','',str)
   #str = re.sub('[\\]','',str)
-  s = re.sub(u'[^A-Za-z0-9 {}$&#_%.!? \xfc \xf6 \xe4 \xdf \xc4 \xd6 \xdc \xdf]','',s)
+  #s = re.sub(u'[^A-Za-z0-9 {}$&#_%.!? \xfc \xf6 \xe4 \xdf \xc4 \xd6 \xdc \xdf]','',s)
+  s = re.sub(u'[^A-Za-z0-9()$&#_ % .:,;!?=äöüÄÖÜß \/ \-]','',s)
   return s
 
+#Escapes certain characters for latex compliance
 def replaceLatexCharacters(str):
-  str = str.replace("{","\\{")
-  str = str.replace("}","\\}")
+  #str = str.replace("{","\\{")
+  #str = str.replace("}","\\}")
   str = str.replace("$","\\$")
   str = str.replace("&","\\&")
   str = str.replace("#","\\#")
@@ -56,11 +67,13 @@ def replaceLatexCharacters(str):
   str = str.replace("%","\\%")
   return str;
 
+#Parses variables and their values from json to latex and writes them to .tex
 def renderPDF(jsonFile):
-  #with open('form.json') as json_data:
+  #Opens json file and loads it to string
   with open(jsonFile) as json_data:
     d = json.load(json_data)
   
+  #Array of boolean variable names
   booleans = [
     "topRadio",
     "topPhone",
@@ -89,7 +102,8 @@ def renderPDF(jsonFile):
     "connectionTickC",
     "connectionTickD",
     "connectionTickE"]
-    
+  
+  #Array of string variable names and their maximum length in latex template
   strings = [
     ["primaryDate",11],
     ["primaryTime",5],
@@ -119,75 +133,68 @@ def renderPDF(jsonFile):
     ["connectionB",7],
     ["connectionC",7],
     ["connectionD",7],
-    ["connectionE",7]
-      ]
+    ["connectionE",7]]
   
+  #Array of multiline string variable names, their maximum number of lines and their maximum number of chars per line
   multiLineStrings = [
     ["content",10,89],
     ["annotations",8,35]]
-
+  
+  #Array of booleans where json variable name and latex variable name are not equal and need translation
+  booleansInconsistentVariableName = [
+    ["stationS1","stationSOne"],
+    ["stationS2","stationSTwo"],
+    ["stationS3","stationSThree"],
+    ["stationS4","stationSFour"],
+    ["stationS6","stationSSix"]]
+    
   VV = ""
-
-  for [str,lineLength] in strings:
-    s = d[str]
+  
+  #Translating string variables from json to latex
+  for [variableName,lineLength] in strings:
+    s = d[variableName]
     s = removeUnwantedCharacters(s)
     s = trimString(s, lineLength)
     s = replaceLatexCharacters(s)
-    VV += "\\def \\" + str + "{" + s + "} "
+    VV += "\\def \\" + variableName + "{" + s + "} "
   
-  for [str,maxLines,lineLength] in multiLineStrings:
-    s = d[str]
+  #Translating multiline string variables from json to latex
+  for [variableName,maxLines,lineLength] in multiLineStrings:
+    s = d[variableName]
     s = removeUnwantedCharacters(s)
     s = adjustString(s,maxLines,lineLength)
     s = replaceLatexCharacters(s)
-    VV += "\\def \\" + str + "{" + s + "} "
+    VV += "\\def \\" + variableName + "{" + s + "} "
     
-  for str in booleans:
-    if d[str]:
-      VV += "\\def \\" + str + "{\\checkedbox}"
+  #Translating boolean variables from json to latex
+  for variableName in booleans:
+    if d[variableName]:
+      VV += "\\def \\" + variableName + "{\\checkedbox}"
     else:
-      VV += "\\def \\" + str + "{\\uncheckedbox}"
+      VV += "\\def \\" + variableName + "{\\uncheckedbox}"
   
+  #Translating boolean variables with inconsistent names from json to latex
+  for [jsonVariableName,latexVariableName] in booleansInconsistentVariableName:
+    if d[jsonVariableName]:
+      VV += "\\def \\" + latexVariableName + "{\\checkedbox}"
+    else:
+      VV += "\\def \\" + latexVariableName + "{\\uncheckedbox}"
   
-  #Special Cases
+  #Special case where Json has one variable and latex template requires two
   if d["outgoing"]:
     VV += "\\def \\incoming{\\uncheckedbox}"
   else:
     VV += "\\def \\incoming{\\checkedbox}"
-    
-  if d["stationS1"]:
-    VV += "\\def \\stationSOne{\\checkedbox}"
-  else:
-    VV += "\\def \\stationSOne{\\uncheckedbox}"
   
-  if d["stationS2"]:
-    VV += "\\def \\stationSTwo{\\checkedbox}"
-  else:
-    VV += "\\def \\stationSTwo{\\uncheckedbox}"
-    
-  if d["stationS3"]:
-    VV += "\\def \\stationSThree{\\checkedbox}"
-  else:
-    VV += "\\def \\stationSThree{\\uncheckedbox}"
   
-  if d["stationS4"]:
-    VV += "\\def \\stationSFour{\\checkedbox}"
-  else:
-    VV += "\\def \\stationSFour{\\uncheckedbox}"
-    
-  if d["stationS6"]:
-    VV += "\\def \\stationSSix{\\checkedbox}"
-  else:
-    VV += "\\def \\stationSSix{\\uncheckedbox}"
-  
+  #Writing generated string to .tex
   with io.open("variables.tex", mode="w", encoding="UTF8") as fd:
     fd.write(VV)
   
+  #Compiling pdf
   os.system("pdflatex VVtest.tex")
   return;
 
 
 if __name__ == "__main__":
-  #renderPDF(sys.argv[1])
-  print(adjustString('Das ist besser',8,5))
-  print(adjustString('Wort zweie',2,4))
+  renderPDF(sys.argv[1])
