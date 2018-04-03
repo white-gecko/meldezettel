508913865ac06ee8210c5c41ef157aa2adcc40ef
Lucas Schons <ls80zyse@studserv.uni-leipzig.de>
2018-04-03 11:42:39 +0200

    finish taborder

--- Projekt/src/frontend/src/components/THWForm.vue
+++ Projekt/src/frontend/src/components/THWForm.vue
@@ -1073,104 +1073,16 @@ export default {
         tempEingehend: true,
         tempAusgehend: false,
         isEdit: false,
-        tabIndexConf: {
-          topRadio: -1,
-          topPhone: -1,
-          topFax: -1,
-          topDFU: -1,
-          topCourier: -1,
-          // incoming input-group
-          incomingDate: -1,
-          incomingTime: -1,
-          incomingHdZ: -1,
-          // outbound input-group
-          // - acceptance-note
-          outboundAccDate: -1,
-          outboundAccTime: -1,
-          outboundAccHdZ: -1,
-          // - transport-note
-          outboundTransDate: -1,
-          outboundTransTime: -1,
-          outboundTransHdZ: -1,
-          // technical-logbook group
-          lbNumber: -1,
-          selectIncoming: -1,
-          selectOutbound: -1,
-          // call-acceptation input
-          receiverName: -1,
-          // mid message-type checkbox-group
-          midRadio: -1,
-          midPhone: -1,
-          midFax: -1,
-          midDFU: -1,
-          midCourier: -1,
-          // announcement/message checkbox-group
-          callAnnouncement: -1,
-          callMessage: -1,
-          // instant/flash checkbox-group
-          priorityInstant: -1,
-          priorityFlash: -1,
-          // call number input
-          callNumber: -1,
-          // address input
-          address: -1,
-          // conversation-note checkbox
-          conversationNote: -1,
-          // content input
-          content: -1,
-          // sender input
-          sender: -1,
-          // time-create input
-          timeCreate: -1,
-          // identification input
-          identification: -1,
-          // position input
-          position: -1,
-          // acknowledgement input-group
-          ackTime: -1,
-          ackIdentification: -1,
-          // TEL/EL/... input-group
-          stationLeader: -1,
-          stationS1: -1,
-          stationS2: -1,
-          stationS3: -1,
-          stationS4: -1,
-          stationS6: -1,
-          // advisers input/checkbox-group
-          advisorA: -1,
-          advisorTickA: -1,
-          advisorB: -1,
-          advisorTickB: -1,
-          advisorC: -1,
-          advisorTickC: -1,
-          advisorD: -1,
-          advisorTickD: -1,
-          advisorE: -1,
-          advisorTickE: -1,
-          // connection input/checkbox-group
-          connectionA: -1,
-          connectionTickA: -1,
-          connectionB: -1,
-          connectionTickB: -1,
-          connectionC: -1,
-          connectionTickC: -1,
-          connectionD: -1,
-          connectionTickD: -1,
-          connectionE: -1,
-          connectionTickE: -1,
-          // annotations input
-          annotations: -1,
-          // send button
-          buttonSend: 2,
-          // reset button
-          buttonReset: 2
-        }
+        tabIndexConf: {}
       }
     }
   },
 
   beforeRouteEnter (to, from, next) {
-    next(vm => vm.loadDocument(to.params.id))
+    next(vm => {
+      vm.loadDocument(to.params.id)
+      vm.setTabIndexConf()
+    })
   },
 
   beforeRouteUpdate (to, from, next) {
@@ -1219,6 +1131,19 @@ export default {
           this.messageBoxError('', error.message)
             .then(this.$router.push({name: 'Home'}))
         })
+    },
+
+    /**
+     * Sets the the tabIndexConf Property
+     */
+    setTabIndexConf: function () {
+      const baseConf = config.tabOrder.base
+      const state = this.formdata.ticketState
+      const stateConf = config.tabOrder[state]
+      // create state-specific tabindex config
+      this.other.tabIndexConf = {}
+      const result = Object.assign({}, baseConf, stateConf)
+      this.other.tabIndexConf = result
     },
 
     loadDocument: function (id) {
@@ -1477,29 +1402,12 @@ export default {
     }
     // End of visibility switches
     // =================================================
-
-    /** Property sets the tabindex value for input element in the form
-     * @returns Object
-     */
-    // tabIndexConf: function () {
-    //   let baseConf = config.tabOrder.base
-    //   let state = this.formdata.ticketState
-    //   let stateConf = config.tabOrder[state]
-    //   // create state-specific tabindex config
-    //   return Object.assign(baseConf, stateConf)
-    // }
   },
+  // watch for changes in the formdata object, trigger setTabIndexConf()
   watch: {
     formdata: {
       handler: function (oldVal, newVal) {
-        const baseConf = config.tabOrder.base
-        const state = this.formdata.ticketState
-        const stateConf = config.tabOrder[state]
-        // create state-specific tabindex config
-        this.other.tabIndexConf = {}
-        const result = Object.assign({}, baseConf, stateConf)
-        console.log(result)
-        this.other.tabIndexConf = result
+        this.setTabIndexConf()
       },
       deep: true
     }
