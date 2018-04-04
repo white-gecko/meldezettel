c2c8556f19d0adfdf6b1c19221f4f181c4d28cf0
Max Schrodt <ms18qame@studserv.uni-leipzig.de>
2018-04-04 16:41:20 +0200

    fixed linting errors

--- Projekt/src/frontend/src/api/PrintServiceAdapter.js
+++ Projekt/src/frontend/src/api/PrintServiceAdapter.js
@@ -5,7 +5,6 @@ import axios from 'axios'
  */
 export const printservice = {
   url: 'http://127.0.0.1:5001/',
-//url: 'http://192.168.2.101:5001/',
   sendData: function (formdataString) {
     return axios.request({
       method: 'post',
