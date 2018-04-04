cff66906717ae1c977ae570f014f3874e4f437da
Lucas Schons <ls80zyse@studserv.uni-leipzig.de>
2018-04-04 16:23:38 +0200

    Edit server startup script. Show possible ipAddr

--- Projekt/src/frontend/server.js
+++ Projekt/src/frontend/server.js
@@ -1,12 +1,30 @@
 // server.js
+// Used to start up an express app that serves our frontend
 
-const express = require('express');
-const serveStatic = require('serve-static');
+const express = require('express')
+const serveStatic = require('serve-static')
+const os = require('os')
 
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
+// get list of network interfaces
+let ifaces = os.networkInterfaces()
+let ipAddrList = []
+// Check for WLAN or ETHERNET network interfaces
+let regEx = /eth\d|wlp\d/
+Object.keys(ifaces).forEach(function (ifname) {
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return
+    }
+    if (regEx.test(ifname)) ipAddrList.push(iface.address)
+  })
+})
 
-let port = process.env.PORT || 8080;
-app.listen(port);
+app = express()
+app.use(serveStatic(__dirname + "/dist"))
 
-console.log('server started '+ port);
+let port = process.env.PORT || 8080
+app.listen(port)
+
+console.log('Server started. Reachable under:\n')
+ipAddrList.forEach((ipAddr) => console.log('http://' + ipAddr + ':' + port))
