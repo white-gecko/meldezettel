0d098b98ead2d4f79037869ba3c855529468f78e
Marc Wahsner <mw83waji@studserv.uni-leipzig.de>
2018-04-04 16:27:25 +0200

    renderserver accessible under localhost again

--- Projekt/src/frontend/src/api/PrintServiceAdapter.js
+++ Projekt/src/frontend/src/api/PrintServiceAdapter.js
@@ -4,8 +4,8 @@ import axios from 'axios'
  * Communication with print service
  */
 export const printservice = {
-//   url: 'http://127.0.0.1:5001/',
-  url: 'http://192.168.2.101:5001/',
+  url: 'http://127.0.0.1:5001/',
+//url: 'http://192.168.2.101:5001/',
   sendData: function (formdataString) {
     return axios.request({
       method: 'post',
--- Projekt/src/renderPDF/testrequest.py
+++ Projekt/src/renderPDF/testrequest.py
@@ -2,8 +2,8 @@ from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 import json
 
-url = 'http://192.168.2.101:5001/'
-# url = 'http://localhost:5001/'
+# url = 'http://192.168.2.101:5001/'
+url = 'http://localhost:5001/'
 
 with open('formData.json') as f:
     formdata = json.load(f)
