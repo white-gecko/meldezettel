331c56ac049f22b0fbec241ceeffad7a122335c2
Lucas Schons <ls80zyse@studserv.uni-leipzig.de>
2018-04-09 12:10:18 +0200

    Add config files for state mapping and input visibility

--- Projekt/src/frontend/src/components/THWForm.vue
+++ Projekt/src/frontend/src/components/THWForm.vue
@@ -990,7 +990,7 @@ import ElHeader from 'element-ui/packages/header/src/main'
 import ElRow from 'element-ui/packages/row/src/row'
 import ElContainer from 'element-ui/packages/container/src/main'
 import ElMain from 'element-ui/packages/main/src/main'
-import config from '../status.config'
+import config from '../config/status.config'
 
 export default {
   components: {
---
+++ Projekt/src/frontend/src/config/stateMapping.config.js
@@ -0,0 +1,32 @@
+/**
+ * Defines the state mapping.
+ * States are being incremented or decremented based on the current state
+ * (left side) and the type of action is being applied to the form (reject,
+ * accept).
+ */
+export default {
+  reject: {
+    1: 2,
+    3: 2,
+    5: 6,
+    7: 6,
+    11: 12,
+    13: 12
+  },
+  accept: {
+    0: 1,
+    1: 3,
+    2: 1,
+    3: 4,
+    4: 5,
+    5: 7,
+    6: 5,
+    7: 8,
+    8: 9,
+    10: 11,
+    11: 13,
+    12: 11,
+    13: 14,
+    14: 15
+  }
+}
